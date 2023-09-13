1c821602aa469639925234e15ea1a923bbd30b55
Husein <husenfadhullah@gmail.com>
2023-09-13 13:30:48 +0800

    Fitur ke 2

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -1,5 +1,7 @@
 public class OurFirstGithubCollab{
     public static void main (String[] args){
         System.out.println("Fitur 1");
+
+        System.out.println("Pengembangan ke 2");
     }
 }
